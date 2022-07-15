d9c5b904aa22a5c7b1d113cb035024accb589f9c
Adarsh Vikram Singh <adarshvikram552002@gmail.com>
2022-07-15 19:44:42 +0530

    event_on_image

---
+++ jav.js
@@ -0,0 +1,7 @@
+const onb = document.getElementById("on").addEventListener("click",()=>{
+    const bulb = document.getElementById("bulb").src = "img/on_image.png";
+})
+
+const offb = document.getElementById("off").addEventListener("click",()=>{
+    const bulb = document.getElementById("bulb").src = "img/off_image.png";
+})
